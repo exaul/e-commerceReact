cbd6396acb9409c8ac2c0defc3173bedaa193e7d
exaul <137360873+exaul@users.noreply.github.com>
2024-06-10 23:20:36 -0500

    ejercicio en clase

---
+++ src/data.jsx
@@ -0,0 +1,63 @@
+import BeanImg from "./assets/ecomm-beanie.png";
+import alpacaImg from "./assets/ecomm-alpaca.png";
+import hoodieImg from "./assets/ecomm-hoodie.png";
+import winterImg from "./assets/ecomm-winter.png";
+import roundneckImg from "./assets/ecomm-round-neck.png";
+import croosbackTmg from "./assets/ecomm-halter.png";
+
+ export const PRODUCTS = [
+  {
+    image: BeanImg,
+    title: "Blue Chunky Beanie",
+    category: "Run, Dress",
+    price: 10.0,
+    wasprice: 15.0,
+    discount: 10,
+    starts: 4,
+  },
+  {
+    image: alpacaImg,
+    title: "Oversized Alpaca Crew",
+    category: "Run, Dress",
+    price: 10.0,
+    wasprice: 15.0,
+    discount: 10,
+    starts: 4,
+  },
+  {
+    image: hoodieImg,
+    title: "Diesel V2 Hoodie",
+    category: "Run, Dress",
+    price: 25.0,
+    wasprice: 45.0,
+    discount: 10,
+    starts: 5,
+  },
+  {
+    image: winterImg,
+    title: "Blue Chunky Beanie",
+    category: "Run, Dress",
+    price: 25.0,
+    wasprice: 45.0,
+    discount: 15,
+    starts: 4,
+  },
+  {
+    image: roundneckImg,
+    title: "Round neck T-shirt",
+    category: "Run, Dress",
+    price: 20.0,
+    wasprice: 30.0,
+    discount: 0,
+    starts: 3,
+  },
+  {
+    image: croosbackTmg,
+    title: "Crossback Halter Dress",
+    category: "Run, Dress",
+    price: 18.0,
+    wasprice: 25.0,
+    discount: 0,
+    starts: 3,
+  },
+];
